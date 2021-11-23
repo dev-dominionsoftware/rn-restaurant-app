f9a816a286b9d28dfc9d619a08c970b2e9d09fcb
Marek Kaszycki <marek.kaszycki@northmill.se>
2021-11-23 15:27:03 +0100

    SearchBar implemeneted

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {View, Text, StyleSheet} from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
-    return <View>
-        <Text>Search screen</Text>
+    return <View style={styles.container}>
+        <SearchBar onSearchChanged={(text) => {
+            console.log(text)
+        }}/>
     </View>
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        padding: 16,
+    },
+});
 
 export default SearchScreen
