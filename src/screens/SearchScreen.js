77b28f37028ef9913a0864964c36faaf38c70450
Marek Kaszycki <marek.kaszycki@northmill.se>
2021-11-24 09:02:11 +0100

    Use axios to fetch data from api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,29 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet} from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
-    const [searchText, setSearchText] = useState('')
+    const [searchText, setSearchText] = useState('');
+    const [results, setResults] = useState([]);
+
+    const searchApi = async () => {
+        const responce = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term: searchText,
+                location: 'san jose',
+            }
+        });
+        setResults(responce.data.businesses);
+    }
 
     return <View style={styles.container}>
-        <SearchBar searchText={searchText} onSearchChanged={(text) => setSearchText(text)} onTextSubmit={() => { console.log('submitted')}}/>
-        <Text>{searchText}</Text>
+        <SearchBar searchText={searchText} onSearchChanged={(text) => setSearchText(text)} onTextSubmit={() => {
+            console.log('submitted')
+            searchApi()
+        }}/>
+        <Text>We hav found {results.length} results</Text>
     </View>
 }
 
